dae26764bcd62e6bc02a5071838859caa3154db2
Raheel_Savani <raheelsavani@gmail.com>
2026-07-18 11:09:14 -0500

    add entry writing with append mode

--- journal.py
+++ journal.py
@@ -1,9 +1,11 @@
-print("Wlecome to your journal. Enter 1 to Add Entry, 2 to View Entries, 3 to Quit")
+print("Welcome to your journal. Enter 1 to Add Entry, 2 to View Entries, 3 to Quit")
 
 while True:
     selection = input("Please enter selection.  ").strip()
     if selection == "1":
-        print("Adding Entry")
+        entry = input("Please type your entry here:  ")
+        with open("entries.txt", "a") as file:
+            file.write(entry + "\n")
     elif selection == "2":
         print("Viewing Entries")
     elif selection == "3":
